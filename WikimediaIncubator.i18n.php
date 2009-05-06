6ca8aef5a3810e5d7e328e207a26d0a6f53e7b9f
Robin Pepermans <robin@users.mediawiki.org>
2009-05-06 16:05:19 +0000

    add German translation by MF-Warburg

--- WikimediaIncubator.i18n.php
+++ WikimediaIncubator.i18n.php
@@ -24,6 +24,35 @@ $messages['en'] = array(
 	'wminc-warning-suggest-move' => 'You can [{{fullurl:Special:MovePage/$3|wpNewTitle=$2}} move this page to $1].',
 );
 
+/** German (Deutsch)
+ * @author MF-Warburg
+ */
+$messages['de'] = array(
+    'wminc-desc' => 'Testwiki-System für den Wikimedia Incubator',
+    'wminc-viewuserlang' => 'Benutzersprache und Testwiki einsehen',
+    'wminc-testwiki' => 'Testwiki:',
+    'wminc-testwiki-none' => 'Keins/Alle',
+    'wminc-prefinfo-language' => 'Sprache deiner Benutzeroberfläche - vom Testwiki unabhängig',
+    'wminc-prefinfo-code' => 'Der ISO-639-Sprachcode',
+    'wminc-prefinfo-project' => 'Das Wikimedia-Projekt, an dem du hier arbeitest ("Incubator" für Benutzer, die allgemeine Aufgaben übernehmen)',
+    'wminc-prefinfo-error' => 'Bei diesem Projekt muss ein Sprachcode angeben werden!',
+    'wminc-warning-unprefixed' => 'Achtung: Du bearbeitest eine Seite ohne Präfix!',
+    'wminc-warning-suggest' => 'Du kannst hier eine Seite erstellen: [[$1]].',
+	'wminc-warning-suggest-move' => 'Du kannst [{{fullurl:Special:MovePage/$3|wpNewTitle=$2}} diese Seite nach $1 verschieben].',
+);
+
+/** German (formal address) (Deutsch (Sie-Form))
+ * @author MF-Warburg
+ */
+$messages['de-formal'] = array(
+	'wminc-prefinfo-language' => 'Sprache Ihrer Benutzeroberfläche - vom Testwiki unabhängig',
+	'wminc-prefinfo-project' => 'Das Wikimedia-Projekt, an dem Sie hier arbeiten ("Incubator" für Benutzer, die allgemeine Aufgaben übernehmen)',
+	'wminc-warning-unprefixed' => 'Achtung: Sie bearbeiten eine Seite ohne Präfix!',
+	'wminc-warning-suggest' => 'Sie können hier eine Seite erstellen: [[$1]].',
+	'wminc-warning-suggest-move' => 'Sie können [{{fullurl:Special:MovePage/$3|wpNewTitle=$2}} diese Seite nach $1 verschieben].',
+);
+
+
 /** Dutch (Nederlands)
  * @author SPQRobin
  */
