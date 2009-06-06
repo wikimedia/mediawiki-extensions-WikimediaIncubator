0f5b6dcff250332d413f50c2b0ee5a2cff89ba0f
Robin Pepermans <robin@users.mediawiki.org>
2009-06-06 16:44:29 +0000

    Use function addExtraSQL() for random page selection

--- SpecialRandomByTest.php
+++ SpecialRandomByTest.php
@@ -7,13 +7,15 @@
 class SpecialRandomByTest extends RandomPage
 {
 	public function __construct() {
-		global $wgExtraRandompageSQL, $wgUser, $wmincPref;
-		if(IncubatorTest::isNormalPrefix()) {
-			$wgExtraRandompageSQL = 'page_title like "W'.$wgUser->getOption($wmincPref . '-project').'/'.$wgUser->getOption($wmincPref . '-code').'/%%"';
-		} elseif($wgUser->getOption($wmincPref . '-project') == 'inc') {
-			$wgExtraRandompageSQL = 'page_title not like "W_/%%" OR "W_/%%/%%"';
-		}
+		parent::__construct( 'RandomByTest' );		
+	}
 
-		parent::__construct( 'RandomByTest' );
+	public function addExtraSQL() {
+		global $wgUser, $wmincPref;
+		if(IncubatorTest::isNormalPrefix()) {
+			return 'page_title like "W'.$wgUser->getOption($wmincPref . '-project').'/'.$wgUser->getOption($wmincPref . '-code').'/%%"';
+		} elseif($wgUser->getOption($wmincPref . '-project') == 'inc') {
+			return 'page_title not like "W_/%%" OR "W_/%%/%%"';
+		}
 	}
 }
