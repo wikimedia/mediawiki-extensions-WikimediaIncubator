c393b01523b462056444cb7969669924ae08a43e
Happy-melon <happy-melon@users.mediawiki.org>
2009-09-07 19:00:49 +0000

    * Totally refactor includes/specials/SpecialUserlogin.php: ** Split backend stuff out into includes/Login.php ** Split account creation and login stuff up, into includes/specials/CreateAccount.php and includes/specials/Userlogin.php. * Reimplement the special pages as subclasses of SpecialPage * Use HTMLForm to generate the input forms ** Deprecate and delete includes/templates/Userlogin.php, which is horrible and old :D
    This changes the syntax of the UserLoginForm and UserCreateForm hooks, and AuthPlugin::modifyUITemplate; they now receive the SpecialPage subclass rather than the template to work with. Update everything I could find in SVN to accommodate this.

--- CreateAccountTestWiki.php
+++ CreateAccountTestWiki.php
@@ -4,14 +4,14 @@
 * This can be used to work with my toolserver project (http://toolserver.org/~robin/?tool=proposewiki), so users don't *have* to change their preferences (automatically is always better :p)
 */
 class AutoTestWiki {
-	function onUserCreateForm( $template ) {
+	function onUserCreateForm( $sp ) {
 		global $wgRequest;
 		$projectvalue = strtolower( $wgRequest->getVal( 'testwikiproject', '' ) );
 		$codevalue = strtolower( $wgRequest->getVal( 'testwikicode', '' ) );
 		if ( preg_match( '/[a-z][a-z][a-z]?/', $codevalue ) && in_array( $projectvalue, array( 'p', 'b', 't', 'q', 'n' ) ) ) {
-			$template->set( 'header', '<input type="hidden" name="testwiki-project" value="' . $projectvalue . '" />
+			$sp->mFormHeader .= '<input type="hidden" name="testwiki-project" value="' . $projectvalue . '" />
 	<input type="hidden" name="testwiki-code" value="' . $codevalue . '" />
-	' );
+	';
 		}
 		return true;
 	}
