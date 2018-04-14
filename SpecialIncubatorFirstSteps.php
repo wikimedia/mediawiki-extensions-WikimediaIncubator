86ec3d95ff17d0b33660893bf6e3f95ac2fe2fe3
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2018-04-14 08:28:01 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 18.0.0
    The following sniffs now pass and were enabled:
    * MediaWiki.Commenting.LicenseComment.InvalidLicenseTag
    
    Change-Id: I857202faeebcde032525031dbef7b7bb35f0708d

--- SpecialIncubatorFirstSteps.php
+++ SpecialIncubatorFirstSteps.php
@@ -8,7 +8,7 @@
  * @file
  * @author Robin Pepermans
  * @author Niklas Laxström
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @license GPL-2.0-or-later
  * @ingroup SpecialPage
  */
 
